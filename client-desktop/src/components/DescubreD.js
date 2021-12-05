a6d4d0d380d38705a8b5bb269cb67a6802129d61
VICTOR LOPEZ GONZALEZ <100405352@alumnos.uc3m.es>
2021-12-05 20:19:34 +0100

    Añadido descubrir películas Desktop y actualiza json de películas

--- client-desktop/src/components/DescubreD.js
+++ client-desktop/src/components/DescubreD.js
@@ -1,12 +1,22 @@
 import React from "react";
+import iron1 from "./images/IronMan(2008).jpeg"
+import iron2 from "./images/IronMan2(2010).jpeg"
+import iron3 from "./images/IronMan3(2013).jpeg"
+import { MdArrowBackIosNew } from "react-icons/md";
+import { MdArrowForwardIos } from "react-icons/md";
 
-export function DescubreD(props){
-    return(
-        <div>
-          <h1>Películas a descubrir</h1>
-          
-          
 
-        </div>
-    );
+export function DescubreD(props) {
+  return (
+    <div className="descubre">
+      <h1>Descubre nuevas películas</h1>
+      <div class="descubre-contenedor">
+        <div class="descubre-flecha-izq"><MdArrowBackIosNew/></div>
+        <div class="descubre-poster-izq"><img src={iron1} alt=""></img></div>
+        <div class="descubre-poster-central"><img src={iron2} alt=""></img></div>
+        <div class="descubre-poster-der"><img src={iron3} alt=""></img></div>
+        <div class="descubre-flecha-der"><MdArrowForwardIos/></div>
+      </div>
+    </div>
+  );
 }
